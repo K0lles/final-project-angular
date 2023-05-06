5ac73ce851ac1716b8970c7a9a0709f8c17749f5
K0lles <oleksijkolotilo63@gmail.com>
2023-05-06 20:22:44 +0300

    beginning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, ValidatorFn, Validators} from '@angular/forms';
+import {MovieService} from "./movie.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,98 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'final-project-angular';
+  isListView: boolean = true;
+  darkMode: boolean = false;
+
+  toggleDarkMode() {
+    this.darkMode = !this.darkMode;
+  }
+
+  fb = new FormBuilder();
+
+  constructor(movieService: MovieService) {
+  }
+
+  ngOnInit() {
+
+  }
+
+  movieForm = this.fb.group({
+    id: [Math.floor(Math.random() * 1000), Validators.required],
+    name: ['', Validators.required],
+    photo: ['', Validators.required],
+    year_of_release: ['', [Validators.required, Validators.pattern('^\\d{4}$')]],
+    box_office: ['', Validators.required],
+    created_at: [new Date().toISOString()]
+  });
+
+  onSubmit(): void {
+    // Save form data to localStorage
+    let temp: string = localStorage.getItem('movies') || '[]';
+    let movies = JSON.parse(temp);
+    movies.push(this.movieForm.value);
+    localStorage.setItem('movies', JSON.stringify(movies));
+
+    // Reset form after submission
+    this.movieForm.reset({
+      id: Math.floor(Math.random() * 1000),
+      created_at: new Date().toISOString()
+    });
+  }
+
+  get movies() {
+    return JSON.parse(localStorage.getItem('movies') || '[]');
+  }
+
+  // Used for validation of the 'year_release' field in FormBuilder
+  yearReleaseValidator(): ValidatorFn {
+    return (control) => {
+      const year = control.value;
+      const currentYear = new Date().getFullYear();
+      const minYear = 1900; // you can set the minimum year as needed
+      const isValid = !isNaN(year) && year >= minYear && year <= currentYear;
+      return isValid ? null : { invalidYear: true };
+    };
+  }
+
+  filmForm = this.fb.group({
+    id: new FormControl({value: '', disabled: true}),
+    photo: ['', Validators.required],
+    name: ['', Validators.required],
+    box_office: ['', Validators.required],
+    created_at: ['', Validators.required],
+  })
+
+
+  // onPhotoSelect(event: Event) {
+  //   // @ts-ignore
+  //   let file = (event.target as HTMLInputElement).files[0];
+  //   if (file) {
+  //     const reader = new FileReader();
+  //     reader.readAsDataURL(file);
+  //     reader.onload = () => {
+  //       this.filmForm.patchValue({
+  //         photo: reader.result || ''
+  //       });
+  //     };
+  //   }
+  // }
+
+  // usersForm = this.fb.group({
+  //   id: new FormControl({value: '', disabled: true}),
+  //   name: ['', Validators.required],
+  //   year_release: ['', [Validators.required, this.yearReleaseValidator()]],
+  //   lastname: ['', Validators.required],
+  //   type: [''],
+  //   email: ['', [Validators.required, Validators.email]],
+  //   password: ['', [Validators.required, Validators.minLength(6), Validators.pattern('^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*()]).+$')]],
+  //   confirmPassword: ['', [Validators.required]],
+  //   subjects: this.fb.array([]),
+  //   description: [''],
+  //   sex: [''],
+  //   phone: ['', [Validators.pattern('^380\\d{9}$')]]
+  //   },
+  //   {
+  //     validators: [this.passwordMatches('password', 'confirmPassword')]
+  //   });
 }
---
+++ src/app/movie.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  constructor() {
+    const movies = [
+      {
+        id: '1',
+        name: 'Batman',
+        photo: 'https://plus.unsplash.com/premium_photo-1672907030852-3eb140df586f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        year_of_release: 2021,
+        box_office: 2000000,
+        created_at: new Date(2023, 4, 21)
+      },
+      {
+        id: '1',
+        name: 'Batman',
+        photo: 'https://plus.unsplash.com/premium_photo-1672907030852-3eb140df586f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        year_of_release: 2021,
+        box_office: 2000000,
+        created_at: new Date(2023, 4, 21)
+      },
+      {
+        id: '1',
+        name: 'Batman',
+        photo: 'https://plus.unsplash.com/premium_photo-1672907030852-3eb140df586f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        year_of_release: 2021,
+        box_office: 2000000,
+        created_at: new Date(2023, 4, 21)
+      },
+      {
+        id: '1',
+        name: 'Batman',
+        photo: 'https://plus.unsplash.com/premium_photo-1672907030852-3eb140df586f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        year_of_release: 2021,
+        box_office: 2000000,
+        created_at: new Date(2023, 4, 21)
+      }
+
+    ]
+
+    let parsed_movies = JSON.stringify(movies);
+    localStorage.setItem('movies', parsed_movies);
+  }
+}
